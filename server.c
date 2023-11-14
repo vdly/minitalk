5bd84d6377b31749a5b7da12e0a941a8055ff13b
Jodene Hii <johii@u80z04s05.42kl.edu.my>
2023-11-14 16:06:43 +0800

    hihihihihi

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: johii <johii@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 16:38:46 by johii             #+#    #+#             */
-/*   Updated: 2023/11/13 21:27:28 by johii            ###   ########.fr       */
+/*   Updated: 2023/11/14 16:00:53 by johii            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,49 @@
 
 void	sig_handler(int signo, siginfo_t *info, void *context)
 {
+	static int	bit_pos;
+	static char	bit_holder;
 
+	(void)context;
+	bit_pos++;
+	bit_holder |= (signo == SIGUSR1);
+	if (bit_pos == 8)
+	{
+		if (bit_holder == '\0')
+		{
+			ft_printf("\n");
+		}
+		else
+			ft_printf("%c", bit_holder);
+		bit_pos = 0;
+		bit_holder = 0;
+	}
+	else
+		bit_holder = bit_holder << 1;
 }
+
+void	miniheader(int num)
+{
+	ft_printf("\n˚₊‧꒰ა ☆ ໒꒱ ‧₊˚˚₊‧꒰ა ☆ ໒꒱ ‧₊˚˚₊‧꒰ა ☆ ໒꒱ ‧₊˚\n");
+	ft_printf("		mini mini mintalk\n");
+	ft_printf("˚₊‧꒰ა ☆ ໒꒱ ‧₊˚˚₊‧꒰ა ☆ ໒꒱ ‧₊˚˚₊‧꒰ა ☆ ໒꒱ ‧₊˚\n");
+	ft_printf("		server pid : %d\n\n", num);
+}
+
 
 int	main(void)
 {
 	struct sigaction	my_sig;
 
-	pid(getpid());
+	miniheader(getpid());
 	my_sig.sa_sigaction = sig_handler;
-	sigemptyset(my_sig.sa_mask);
+	sigemptyset(&my_sig.sa_mask);
 	my_sig.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &my_sig, 0);
 	sigaction(SIGUSR2, &my_sig, 0);
 	while (1)
 		pause();
 	return (0);
-}
-
-void	pid(int num)
-{
-	ft_printf("˚₊‧꒰ა ☆ ໒꒱ ‧₊˚˚₊‧꒰ა ☆ ໒꒱ ‧₊˚˚₊‧꒰ა ☆ ໒꒱ ‧₊˚\n");
-	ft_printf("	mini mini mintalk\n");
-	ft_printf("˚₊‧꒰ა ☆ ໒꒱ ‧₊˚˚₊‧꒰ა ☆ ໒꒱ ‧₊˚˚₊‧꒰ა ☆ ໒꒱ ‧₊˚\n");
-	ft_printf("server pid : %d\n", num);
 }
 
 //by assigning sa_sigaction u could use a custom function
--- server_bonus.c
+++ server_bonus.c
@@ -6,7 +6,7 @@
 /*   By: johii <johii@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 20:19:40 by johii             #+#    #+#             */
-/*   Updated: 2023/11/13 20:19:53 by johii            ###   ########.fr       */
+/*   Updated: 2023/11/14 16:04:17 by johii            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,50 @@
 #include "libft/libft.h"
 #include <signal.h>
 
+void	sig_handler(int signo, siginfo_t *info, void *context)
+{
+	static int	bit_pos;
+	static char	bit_holder;
+
+	(void)context;
+	bit_pos++;
+	bit_holder |= (signo == SIGUSR1);
+	if (bit_pos == 8)
+	{
+		if (bit_holder == '\0')
+		{
+			kill(info->si_pid, SIGUSR1);//
+			ft_printf("\n");
+		}
+		else
+			ft_printf("%c", bit_holder);
+		bit_pos = 0;
+		bit_holder = 0;
+	}
+	else
+		bit_holder = bit_holder << 1;
+}
+
+void	miniheader(int num)
+{
+	ft_printf("\n˚₊‧꒰ა ☆ ໒꒱ ‧₊˚˚₊‧꒰ა ☆ ໒꒱ ‧₊˚˚₊‧꒰ა ☆ ໒꒱ ‧₊˚\n");
+	ft_printf("	mini mini mintalk\n");
+	ft_printf("˚₊‧꒰ა ☆ ໒꒱ ‧₊˚˚₊‧꒰ა ☆ ໒꒱ ‧₊˚˚₊‧꒰ა ☆ ໒꒱ ‧₊˚\n");
+	ft_printf("server pid : %d\n\n", num);
+}
+
+int	main(void)
+{
+	struct sigaction	my_sig;
+
+	pid(getpid());
+	my_sig.sa_sigaction = sig_handler;
+	sigemptyset(my_sig.sa_mask);
+	my_sig.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &my_sig, 0);
+	sigaction(SIGUSR2, &my_sig, 0);
+	while (1)
+		pause();
+	return (0);
+}
 
